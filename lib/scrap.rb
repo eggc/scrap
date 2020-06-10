cd99636d6cdcddcea8397ef6ce6d02d36ae17ada
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2020-06-10 23:03:57 +0900

    Install nokogiri

--- lib/scrap.rb
+++ lib/scrap.rb
@@ -1,11 +1,12 @@
 require "scrap/version"
 require "open-uri"
+require "nokogiri"
 
 module Scrap
   class Error < StandardError; end
 
   def self.main(url)
     raw_html = open(url).read
-    puts(raw_html)
+    puts Nokogiri::HTML.parse(raw_html)
   end
 end
