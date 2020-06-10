38eeb7b40ac3d12f92d336d4d6866787d33ff7a1
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2020-06-10 23:00:00 +0900

    Add open-uri to read html

--- lib/scrap.rb
+++ lib/scrap.rb
@@ -1,9 +1,11 @@
 require "scrap/version"
+require "open-uri"
 
 module Scrap
   class Error < StandardError; end
 
-  def self.main(options)
-    puts "hello #{options}"
+  def self.main(url)
+    raw_html = open(url).read
+    puts(raw_html)
   end
 end
