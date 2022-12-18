4190a9da1176c607d0af44d3a29c82a483a72808
eggc <no.eggchicken@gmail.com>
2022-12-18 10:41:30 +0900

    ✨ feat: `query` can receive multiple attributes

--- lib/scrap/html.rb
+++ lib/scrap/html.rb
@@ -9,21 +9,23 @@ class Scrap
     end
 
     def title
-      query(selector: 'title', attribute: :inner_text).first
+      query(selector: 'title', attributes: :inner_text).first[:inner_text]
     end
 
     def image_urls
-      query(selector: 'img', attribute: 'src').compact
+      query(selector: 'img', attributes: 'src').map { _1['src'] }.compact
     end
 
-    def query(selector:, attribute:)
-      elements = @html
-      elements = @html.css(selector) if selector
-
-      if attribute == :inner_text
-        elements.map(&:inner_text)
-      else
-        elements.map { |element| element.get_attribute(attribute) }
+    def query(selector:, attributes:)
+      @html.css(selector).map do |element|
+        Array(attributes).each_with_object({}) do |attribute, hash|
+          hash[attribute] =
+            if attribute == :inner_text
+              element.inner_text
+            else
+              element.get_attribute(attribute)
+            end
+        end
       end
     end
   end
